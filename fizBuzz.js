50fa82eca4704b9195e0fbb530f0ec8d0fabca3e
Ainul Yaqin <Akinpasha82@gmail.com>
2022-08-01 21:52:48 +0700

    solve fixBuzz

---
+++ fizBuzz.js
@@ -0,0 +1,13 @@
+// Task 1 - FizzBuzz
+// Buat loop yang mencetak angka 1-100, JIKA kelipatan 3 maka outputnya "Fizz" , JIKA kelipatan 5 outputnya "Buzz" dan JIKA kelipatan 3 dan 5 keluarkan 'FizzBuzz'
+
+const fizBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % (3 * 5) === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
+};
+
+fizBuzz();
